48fd708687d4e7c29f11b12819bed5fcfa072f8f
JDbuild <2749388+jdbuild@users.noreply.github.com>
2026-03-16 18:55:44 +0100

    added comment

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+
+# this is needed to make sure that the streamlit command is available in the PATH
 export PATH="$HOME/.local/bin:$PATH"
 exec streamlit run app.py "$@"
